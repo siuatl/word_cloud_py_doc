7cf3a0904fb527926ea3d11a3fce5866721173e1
Leticia Hernandez Bautista <170028349+siuatl@users.noreply.github.com>
2025-03-06 21:32:43 -0600

    made the argument required

--- pdwc.py
+++ pdwc.py
@@ -1,6 +1,8 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--sqlite-db", help="a data base")
+parser = argparse.ArgumentParser(
+    description="This program allows the user to generate a word cloud from the Python documentation.")
+parser.add_argument("-f", "--sqlite-db", required=True,
+                    help="path to a SQLite database file")
 args = parser.parse_args()
 if args.sqlite_db:
     print(f"{args.sqlite_db}")
