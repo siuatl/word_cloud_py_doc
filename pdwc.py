f309185066f9db7dbe196eff6e114474b6411a45
Leticia Hernandez Bautista <170028349+siuatl@users.noreply.github.com>
2025-03-25 20:07:53 -0600

    ordered dictionary by values and built a new list with the first 100 words and also built a list with not required words

--- pdwc.py
+++ pdwc.py
@@ -9,7 +9,18 @@ parser.add_argument("-f", "--sqlite-db", required=True,
 args = parser.parse_args()
 db_con = sqlite3.connect(args.sqlite_db)
 cursor = db_con.cursor()
-cursor.execute('SELECT * FROM Pages LIMIT 10 OFFSET 100')
+cursor.execute('SELECT * FROM Pages LIMIT 10')
+w_not_required = ['also', 'added', 'about', 'arguments', 'been', 'bytes', 'called', 'changed', 'them', 'want', 'provides', 'window',
+                  'changes', 'call', 'current', 'contributed', 'characters', 'document', 'does', 'example', 'each', 'written', 'provided',
+                  'from', 'file', 'functions', 'first', 'following', 'files', 'given', 'have', 'information', 'instance', 'instead', 'into',
+                  'line', 'like', 'methods', 'must', 'more', 'modules', 'message', 'name', 'note', 'next', 'names', 'objects', 'because',  'exceptions', 'just',
+                  'only', 'other', 'previous', 'returns', 'returned', 'raised', 'release', 'should', 'same', 'some', 'being', 'were', 'whether', 'many',
+                  'supports', 'such', 'specified', 'this', 'that', 'there', 'than', 'these', 'they', 'then', 'types', 'time', 'used', 'using',
+                  'values', 'with', 'will', 'when', 'which', 'standard', 'updated', 'builtin', 'format', 'sequence', 'where', 'your', 'directory', 'defined',
+                  'windows', 'available', 'attributes', 'after', 'always', 'before', 'base', 'both', 'between', 'classes', 'created', 'containing',
+                  'calls', 'calling', 'contains', 'different', 'either', 'equivalent', 'form', 'found', 'link', 'most', 'errors',
+                  'mode', 'make', 'need',  'optional', 'options', 'otherwise', 'passed', 'process', 'possible', 'strings', 'parameters', 'variables',
+                  'suggesting', 'since', 'size', 'single', 'section', 'text', 'test', 'their', 'uses', 'useful', 'without', 'would']
 table = dict()
 for row in cursor:
     data = row[2]
@@ -25,9 +36,25 @@ for row in cursor:
         if len(text_s) == 0:
             continue
         words = text_s.split()
-        print(words)
+        # print(words)
         for word in words:
-            if not word.isalpha() or len(word) < 2:
+            if word.startswith('http'):
+                continue
+            if not word.isalpha() or len(word) < 4:
+                continue
+            if word in w_not_required:
                 continue
             table[word] = table.get(word, 0) + 1
-print(table)
+# table_sor = dict(sorted(table.items(), key=lambda item: item[1], reverse=True))
+table_sor = sorted(table, key=table.get, reverse=True)
+
+# print(table_sor, '\n')
+
+bigger = None
+# lower = None
+for k in table_sor[:100]:
+    if bigger is None or bigger < table_sor[:100]:
+        bigger = table_sor[:100]
+    # if lower is None or lower > table_sor[:100]:
+    #     lower = table_sor[:100]
+print('Range of table:', bigger)
